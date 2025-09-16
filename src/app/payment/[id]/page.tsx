c16927b962818c40dab77f31393dde75cf3fb924
Prashant Timilsina <157892231+PrashantTimilsina@users.noreply.github.com>
2025-09-16 15:37:08 +0545

    Update page.tsx

--- src/app/payment/[id]/page.tsx
+++ src/app/payment/[id]/page.tsx
@@ -96,12 +96,12 @@ function Payment() {
         <input
           type="hidden"
           name="success_url"
-          value={`${process.env.NEXTAUTH_URL}/success/${id}`}
+          value={`https://tour-virid-two.vercel.app/success/${id}`}
         />
         <input
           type="hidden"
           name="failure_url"
-          value={`${process.env.NEXTAUTH_URL}/failure`}
+          value="https://tour-virid-two.vercel.app/failure"
         />
         <input
           type="hidden"
